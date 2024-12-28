13c38921cecc5697d0227abdde5c59f5d42682df
bear_y <Yuri_Ch@mail.com>
2024-12-28 19:25:46 +0200

    Conteinerisation

--- app/whisper/whisper_entry_point.py
+++ app/whisper/whisper_entry_point.py
@@ -1,18 +1,15 @@
 import sys
 import whisper
 
-# Перевірка наявності аргументу
-if len(sys.argv) < 2:
-    sys.exit(0)
-
 # Завантаження моделі
 model = whisper.load_model("turbo")
 
 # Аудіофайл передається як параметр
-audio_file = sys.argv[1]
-
+if len(sys.argv) >= 2:
+    audio_file = sys.argv[1]
 # Транскрибування
-result = model.transcribe(audio_file)
-
+    result = model.transcribe(audio_file)
 # Виведення лише тексту та мови
-print(f'{{"text": "{result["text"]}", "language": "{result["language"]}"}}')
+    print(f'{{"text": "{result["text"]}", "language": "{result["language"]}"}}')
+
+else print('file path not found')
