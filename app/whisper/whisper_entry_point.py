fd1c62830fc0e85626a50f3039f01d453108fae0
YuriChlek <Yuri_Ch@mail.com>
2025-01-13 11:46:07 +0200

    Docker test

--- app/whisper/whisper_entry_point.py
+++ app/whisper/whisper_entry_point.py
@@ -1,16 +1,23 @@
 import sys
 import whisper
 
-# Завантаження моделі
-model = whisper.load_model("turbo")
+try:
+    # Завантаження моделі
+    model = whisper.load_model("turbo")
+except Exception as e:
+    print(f'{{"text": "", "language": "", "error": "Error loading Whisper model: {e}"}}')
+    sys.exit(1)
 
-# Аудіофайл передається як параметр
+# Перевірка аргументів командного рядка
 if len(sys.argv) >= 2:
     audio_file = sys.argv[1]
-# Транскрибування
-    result = model.transcribe(audio_file)
-# Виведення лише тексту та мови
-    print(f'{{"text": "{result["text"]}", "language": "{result["language"]}"}}')
-
+    try:
+        # Спроба транскрибувати аудіофайл
+        result = model.transcribe(audio_file)
+        print(f'{{"text": "{result["text"]}", "language": "{result["language"]}", "error": ""}}')
+    except FileNotFoundError:
+        print(f'{{"text": "", "language": "", "error": "Error: Audio file not found."}}')
+    except Exception as e:
+        print(f'{{"text": "", "language": "", "error": "Error during transcription: {e}"}}')
 else:
-    print('file path not found')
+    print(f'{{"text": "", "language": "", "error": "Error: No audio file path provided as a command-line argument."}}')
